ade1ac2df077d73b6e559aa9fac506281ff8e58a
Gregory Verspecht <gregory.verspecht@gmail.com>
2024-12-17 02:49:16 +0100

    flask PWA

--- src/dev/app/app.py
+++ src/dev/app/app.py
@@ -1,5 +1,12 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, render_template, send_from_directory, make_response
+
 app = Flask(__name__, static_url_path='/static', template_folder='../templates')
+
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Service-Worker-Allowed'] = '/'
+    return response
 
 @app.route('/')
 def home():
@@ -7,7 +14,9 @@ def home():
 
 @app.route('/static/<path:path>')
 def static_files(path):
-    return send_from_directory('static', path)
+    response = make_response(send_from_directory('static', path))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8443, ssl_context=("/home/mona/ssl/cert.pem", "/home/mona/ssl/key.pem"))
