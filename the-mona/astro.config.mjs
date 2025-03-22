ab815eeffefd85a481857680e51663b4570030f2
Gregory Verspecht <gregory.verspecht@gmail.com>
2025-03-22 23:44:59 +0100

    new site

--- the-mona/astro.config.mjs
+++ the-mona/astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [tailwind(), react()],
+  output: 'static',
+  site: 'https://the-mona.gv-tech.eu',
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
