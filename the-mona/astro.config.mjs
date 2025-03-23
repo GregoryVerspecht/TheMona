e785e8aeda745c958e298a9d3cd644c27c159a22
Gregory Verspecht <gregory.verspecht@gmail.com>
2025-03-23 11:20:16 +0100

    new workflow

--- the-mona/astro.config.mjs
+++ the-mona/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static',
-  site: 'https://the-mona.gv-tech.eu',
+  site: 'https://gregoryverspecht.github.io/',
+  base: 'TheMona/the-mona',
 
 });
