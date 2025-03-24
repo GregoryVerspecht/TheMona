ea6a2249015c84117e1d6c4c3944fbbf4acdacb7
Gregory Verspecht <gregory.verspecht@gmail.com>
2025-03-24 20:51:07 +0100

    Updates

--- src/dev/app/maintenance/playsound.py
+++
@@ -1,7 +0,0 @@
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("/home/mona/the-mona/static/assets/sounds/sound-of-the-police.wav")
-pygame.mixer.music.play()
-
-while pygame.mixer.music.get_busy():
-    pass
